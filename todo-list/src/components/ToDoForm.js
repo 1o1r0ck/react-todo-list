987d36178e41b10af94baafaafff725e8fb7b649
1o1r0ck <ilyavk310@gmail.com>
2022-08-03 17:45:18 +0300

    final version

---
+++ todo-list/src/components/ToDoForm.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const ToDoForm = ({ addTask }) => {
+
+    const [userInput, setUserInput] = useState('');
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask(userInput);
+        setUserInput("");
+    };
+
+    
+    
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input 
+                placeholder='Введите задачу...'
+                value={userInput}
+                type="text"
+                onChange={handleChange}
+            
+            />
+            <button> Сохранить </button>
+
+        </form>
+
+
+    )
+}
+
+export default ToDoForm
